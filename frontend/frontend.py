ffd11cfc12f45b0d78124e9a3cffd02950bd6e52
Kevin Lee <kevinlee7007@gmail.com>
2022-06-09 13:18:51 +0200

    moved csv file to same level as frontend

--- frontend/frontend.py
+++ frontend/frontend.py
@@ -9,7 +9,8 @@ st.markdown("""# Pro-Test
 Please enter your expected or live protest conditions below to allow the model to predict the expectation of outcomes"""
             )
 
-file_path = '../pro-test/data/full_df.csv'
+# file_path = '../pro-test/data/full_df.csv'
+file_path = 'full_df.csv'
 df = pd.read_csv(file_path, index_col=0)
 
 country_lst = df['country'].drop_duplicates()
