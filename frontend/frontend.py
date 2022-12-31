baed801d680dfe591367916c7c59a13c28568ab8
Noah Darwich <noah95darwish@gmail.com>
2022-12-31 16:46:42 +0100

    thing

--- frontend/frontend.py
+++ frontend/frontend.py
@@ -11,7 +11,7 @@ st.markdown("""# Pro-Test
 Please enter your expected or live protest conditions below to allow the model to predict the expectation of outcomes"""
             )
 
-file_path = 'data/full_df.csv'
+file_path = './frontend/data/full_df.csv'
 df = pd.read_csv(file_path, index_col=0)
 
 country_lst = df['country'].drop_duplicates()
